c873bb492859cb589dd9a97c16c7b4d4cb326702
DmitriyNenashev <abix5@yandex.ru>
2022-04-15 10:41:56 +0300

    Homework 5.4 + explanation

---
+++ src-05-virt-04-docker-compose/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gstis6eh7ondu9q0io"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g2v6j97gvmcsq8m3nv"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8v3qmm14ttn48keuch"
+}
